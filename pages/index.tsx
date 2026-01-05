fd8e9527e1390761f69b8dddc0de883622b5b1dd
narcisovasconcellos-cloud <narcisovasconcellos@gmail.com>
2026-01-04 23:37:17 -0300

    Update index.tsx
    add basic city state (day and population)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+
 export default function Home() {
+  const [day, setDay] = useState(1);
+  const [population, setPopulation] = useState(1);
+
   return (
-    <main style={{ padding: 40 }}>
+    <main style={{ padding: 20 }}>
       <h1>MyMiniWorld</h1>
-      <p>Sua cidade começa aqui.</p>
+
+      <p>Dia: {day}</p>
+      <p>Habitantes: {population}</p>
+
+      <button onClick={() => setDay(day + 1)}>
+        Passar o dia
+      </button>
     </main>
   );
 }
