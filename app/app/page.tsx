cc80ed37924c6a479e7052f2150c4512130f5a2e
narcisovasconcellos-cloud <narcisovasconcellos@gmail.com>
2026-01-04 22:35:08 -0300

    Create page.tsx

---
+++ app/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>MyMiniWorld</h1>
+      <p>Sua cidade começa aqui.</p>
+      <p>(Em breve: criar cidade e contar visitas)</p>
+    </main>
+  );
+}
