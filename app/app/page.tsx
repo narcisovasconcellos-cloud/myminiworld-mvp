a75f28f04dbccea6583b9bcf3fbf880fa3ef2baf
narcisovasconcellos-cloud <narcisovasconcellos@gmail.com>
2026-01-04 22:50:55 -0300

    Delete app/app directory

--- app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>MyMiniWorld</h1>
-      <p>Sua cidade começa aqui.</p>
-      <p>(Em breve: criar cidade e contar visitas)</p>
-    </main>
-  );
-}
